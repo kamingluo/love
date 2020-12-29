d661a40e8dd5b455cfc61058350926fe273b9027
kaming <954087620@qq.com>
2020-12-29 23:14:23 +0800

    优化

--- wxminiapp/pages/qrcode/qrcode.js
+++ wxminiapp/pages/qrcode/qrcode.js
@@ -102,4 +102,23 @@ Page({
     //console.log("打开设置")
     wx.openSetting()
   },
+
+  /**
+ * 用户点击右上角分享
+ */
+  onShareAppMessage: function () {
+    var name = wx.getStorageSync('userdata').nickName;
+    var userid = wx.getStorageSync('userdata').id
+    return {
+      title: "你的好友" + name +"邀请你回答问题啦",
+      desc: "你的好友" + name + "邀请你回答问题啦",
+      // imageUrl: baseConfig.imageurl+'miniapp/images/appicon.png',
+      imageUrl: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimgres.sj88.com%2Fsj88%2F146%2F726094-202004261141445ea57378bc0a9.jpg&refer=http%3A%2F%2Fimgres.sj88.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1611846557&t=a13f37a0ce90a530792cd8d2f20955f3',
+      path: '/pages/question/question?channel=1001&answer_userid=' + userid, // 路径，传递参数到指定页面。
+    }
+
+  },
+
+
+
 })
